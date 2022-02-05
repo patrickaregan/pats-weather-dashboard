453ea241676bcd620a8dfa66fc8e26ceb52e4b07
patrickaregan <patrickaregan@gmail.com>
2022-02-04 21:08:26 -0600

    close Current weather conditions issue

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,10 @@
 var searchFormEl = $("#search-form");
 var cityNameEl = $(".cityName");
 var weatherIcon = $(".weatherIcon");
+var cityTempEl = $(".cityTemp");
+var cityWindEl = $(".cityWind");
+var cityHumidityEl = $(".cityHumidity");
+var cityUVIndexEl = $(".cityUVIndex");
 var currentDate = moment().format("l");
 
 
@@ -15,6 +19,9 @@ function handleFormSubmit(event) {
 
     // get search input values
     var searchString = $('input[name="city"]').val();
+    if (!searchString.trim() ) {
+        alert("Please enter a city!");
+    }
     var searchTokens = [];
     if (searchString.includes(",")) {
         searchTokens = searchString.trim().split(",");
@@ -24,26 +31,29 @@ function handleFormSubmit(event) {
     }
     var city = searchTokens[0].trim();
     var state = searchTokens[1].trim();
-    var country = "US";
+    var country = "";
 
     // get coordinates and location name
     var lattitude = 0;
     var longitude = 0;
     var location = "";
-    var apiURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "," + state + "," + country + "&limit=1&appid=41acdc298e9f5bb9684ae3f719dae78a";
+    var apiURL = "https://api.openweathermap.org/geo/1.0/direct?q=" + city + "," + state + "," + country + "&limit=1&appid=41acdc298e9f5bb9684ae3f719dae78a";
     fetch(apiURL)
     .then(function(response) {
         // request was successful
         if (response.ok) {
             response.json().then(function(data) {
-                lattitude = data[0].lat;
-                longitude = data[0].lon;
-                location = data[0].name + ", " + data[0].state;
-                console.log("Lat: " + lattitude);
-                console.log("Lon: " + longitude);
-                console.log("Loc: " + location);
-                cityNameEl.text(location + " (" + currentDate + ")");
-
+                try {
+                    lattitude = data[0].lat;
+                    longitude = data[0].lon;
+                    location = data[0].name + ", " + data[0].state;
+                    console.log("Lat: " + lattitude);
+                    console.log("Lon: " + longitude);
+                    console.log("Loc: " + location);
+                    cityNameEl.text(location + " (" + currentDate + ")");
+                } catch(error) {
+                    alert("Sorry, we could not find any data for that city!");
+                }
                 // get weather data
                 var apiURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lattitude + "&lon=" + longitude + "&exclude=alerts&units=imperial&lang=en&appid=41acdc298e9f5bb9684ae3f719dae78a";
                 fetch(apiURL)
@@ -52,10 +62,25 @@ function handleFormSubmit(event) {
                     if (response.ok) {
                         response.json().then(function(data) {
                             console.log(data);
-                            console.log(data.current.weather[0].icon);
+                            console.log(data.current.temp);
                             var icon = data.current.weather[0].icon;
                             var iconDesc = data.current.weather[0].description;
-                            weatherIcon.html("<img src='http://openweathermap.org/img/wn/" + icon + "@2x.png' width='50px' height='50px' alt='" + iconDesc + "'>");
+                            var temp = "Temp: " + data.current.temp + "&deg;" + "F";
+                            var wind = "Wind: " + data.current.wind_speed + " MPH";
+                            var humidity = "Humidity: " + data.current.humidity + " %";
+                            var uvindex = data.current.uvi;
+                            weatherIcon.html("<img src='https://openweathermap.org/img/wn/" + icon + "@2x.png' width='50px' height='50px' alt='" + iconDesc + "'>");
+                            cityTempEl.html(temp);
+                            cityWindEl.html(wind);
+                            cityHumidityEl.html(humidity);
+                            if (parseFloat(uvindex) <= 2) {
+                                cityUVIndexEl.html("UV Index: <span class='bg-success colorCodedUVIndex'>" + uvindex + "</span>");
+                            } else if (parseFloat(uvindex) <= 8) {
+                                cityUVIndexEl.html("UV Index: <span class='bg-warning colorCodedUVIndex'>" + uvindex + "</span>");
+                            } else {
+                                cityUVIndexEl.html("UV Index: <span class='bg-danger colorCodedUVIndex'>" + uvindex + "</span>");
+                            }
+                            
                         })
                     } else {
                         console.log('Error: Weather Data Not Found');
