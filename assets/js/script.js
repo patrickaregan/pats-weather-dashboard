e9ad086a8298829078aaf461ae8e9b83860bde6f
patrickaregan <patrickaregan@gmail.com>
2022-02-04 19:17:11 -0600

    put fetch calls in handleFormSubmit

---
+++ assets/js/script.js
@@ -0,0 +1,81 @@
+// My API key for Open Weather API: 41acdc298e9f5bb9684ae3f719dae78a
+// Geocoding API: http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
+// One Call API:  https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
+
+var searchFormEl = $('#search-form');
+
+function handleFormSubmit(event) {
+
+    // prevent default form submit behavior
+    event.preventDefault();
+
+    // get search input values
+    var searchString = $('input[name="city"]').val();
+    var searchTokens = [];
+    if (searchString.includes(",")) {
+        searchTokens = searchString.trim().split(",");
+    } else {
+        searchTokens[0] = searchString.trim();
+        searchTokens[1] = "";
+    }
+    var city = searchTokens[0].trim();
+    var state = searchTokens[1].trim();
+    var country = "US";
+
+    // get coordinates and location name
+    var lattitude = 0;
+    var longitude = 0;
+    var location = "";
+    var apiURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "," + state + "," + country + "&limit=1&appid=41acdc298e9f5bb9684ae3f719dae78a";
+    fetch(apiURL)
+    .then(function(response) {
+        // request was successful
+        if (response.ok) {
+            response.json().then(function(data) {
+                lattitude = data[0].lat;
+                longitude = data[0].lon;
+                location = data[0].name + ", " + data[0].state;
+                console.log("Lat: " + lattitude);
+                console.log("Lon: " + longitude);
+                console.log("Loc: " + location);
+
+                // get weather data
+                var apiURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lattitude + "&lon=" + longitude + "&exclude=alerts&units=imperial&lang=en&appid=41acdc298e9f5bb9684ae3f719dae78a";
+                fetch(apiURL)
+                .then(function(response) {
+                    // request was successful
+                    if (response.ok) {
+                        response.json().then(function(data) {
+                            console.log(data);
+                        })
+                    } else {
+                        console.log('Error: Weather Data Not Found');
+                    }
+                })
+                .catch(function(error) {
+                    console.log("Unable to connect to Open Weather One Call API");
+                })
+                // end get weather data
+            })
+        } else {
+            console.log('Error: City Not Found');
+        }
+    })
+    .catch(function(error) {
+        console.log("Unable to connect to Open Weather Geocoding API");
+    });
+    // end get coordinates and location name
+
+}
+
+searchFormEl.on("submit", handleFormSubmit);
+
+
+
+
+
+
+
+
+
+
